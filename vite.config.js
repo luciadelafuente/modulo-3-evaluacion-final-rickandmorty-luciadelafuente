71ad5d71b75770db878bde59660016dfc812fe58
luciadelafuente <luciadelaf1991@gmail.com>
2024-08-06 11:38:34 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/modulo-3-evaluacion-final-luciadelafuente/",
+  base: "/modulo-3-evaluacion-final-rickandmorty-luciadelafuente/",
 })
